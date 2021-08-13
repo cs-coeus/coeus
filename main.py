f6b1e746aeb6f8360e9f824debfaca759822e892
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-08-13 21:44:35 +0700

    refactor: rename variable to snake_case

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ test_text = 'When completed in 2023, the CENI facility will become a prototype "
 test_text_qa = 'Apple is a fruit. Apple is red. Apple is delicious.'
 test_text_key = 'KMUTT is a university'
 
-inputP = InputPreparator()
-print(inputP.normalize_text_from_wikipedia("""= test1 = \n 123"""))
+input_p = InputPreparator()
+print(input_p.normalize_text_from_wikipedia("""= test1 = \n 123"""))
 
-summarizerModel = ModelSummarizer()
-print(summarizerModel.predict(test_text))
+summarizer_model = ModelSummarizer()
+print(summarizer_model.predict(test_text))
 
-qaModel = ModelQA()
+qa_model = ModelQA()
 tuple_test = (test_text_qa, ('Apple', 0.6), ['What is '])
 print(tuple_test)
-print(qaModel.predict(tuple_test))
+print(qa_model.predict(tuple_test))
 
-keybertModel = ModelKeyBert()
-print(keybertModel.predict(test_text_key))
+keybert_model = ModelKeyBert()
+print(keybert_model.predict(test_text_key))
 
 sents = ['Obama speaks to the media in Illinois', 'The president greets the press in Chicago',
          'Obama speaks to the media in Illinois', 'The president greets the press in Chicago',
@@ -35,25 +35,25 @@ sents = ['Obama speaks to the media in Illinois', 'The president greets the pres
          'The president greets the press in Chicago', 'The president greets the press in Chicago',
          'Oranges are my favorite fruit']
 
-clusteringModel = ModelClustering()
+clustering_model = ModelClustering()
 # data = [sent.text for sent in sents]
 data = sents
 X = np.arange(len(data)).reshape(-1, 1)
 
 
 def distance(x, y):
-    return ModelClustering.wmd.wmdistance(inputP.preprocess_senetence_to_arr(data[int(x[0])]),
-                                          inputP.preprocess_senetence_to_arr(data[int(y[0])]))
+    return ModelClustering.wmd.wmdistance(input_p.preprocess_senetence_to_arr(data[int(x[0])]),
+                                          input_p.preprocess_senetence_to_arr(data[int(y[0])]))
 
 
 proximity_matrix = pairwise_distances(X, X, metric=distance)
-best_k, best_cluster = clusteringModel.predict((X, len(sents), proximity_matrix))
+best_k, best_cluster = clustering_model.predict((X, len(sents), proximity_matrix))
 
 print(best_k, best_cluster)
 
-spacyModel = ModelSpacy()
-print(list(spacyModel.predict(test_text).sents))
+spacy_model = ModelSpacy()
+print(list(spacy_model.predict(test_text).sents))
 
-wikiRepo = WikiRepository()
+wiki_repo = WikiRepository()
 print(InputPreparator.normalize_text_from_wikipedia(
     WikiRepository.getData("""King_Mongkut's_University_of_Technology_Thonburi""")))
